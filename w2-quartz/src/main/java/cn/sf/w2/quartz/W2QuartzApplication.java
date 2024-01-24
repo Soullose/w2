cc5fabe18c6c232f4eea7999175e182e52aaf71d
WangSF <soullose@qq.com>
2024-01-24 10:39:43 +0800

    update pom and add quartz

---
+++ w2-quartz/src/main/java/cn/sf/w2/quartz/W2QuartzApplication.java
@@ -0,0 +1,11 @@
+package cn.sf.w2.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2QuartzApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(W2QuartzApplication.class, args);
+    }
+}
