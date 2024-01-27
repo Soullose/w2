58239556610e9d71aa1cd2f14be53ff45c8a3cdd
WangSF <soullose@qq.com>
2024-01-27 20:24:10 +0800

    update

---
+++ w2-doc/src/main/java/cn/sf/w2/doc/W2DocApplication.java
@@ -0,0 +1,11 @@
+package cn.sf.w2.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2DocApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(W2DocApplication.class);
+    }
+}
