aa0193821a8c474da92853c1723477eb977221ab
WangSF <soullose@qq.com>
2024-01-28 17:35:37 +0800

    update

---
+++ w2-core/src/main/java/cn/sf/w2/core/W2CoreApplication.java
@@ -0,0 +1,11 @@
+package cn.sf.w2.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2CoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(W2CoreApplication.class);
+    }
+}
